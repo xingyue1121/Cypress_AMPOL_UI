35a909acaf8ed7b4c64a02e90e450692ee6da322
PPL <PPL@192-168-1-13.tpgi.com.au>
2023-07-30 22:13:25 +1000

    remove comments

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -6,8 +6,6 @@ import EnergyPostcodePage from '../support/PageObjects/EnergyPostcodePage';
 describe('UI Test', () => {
 
   before(() => {
-    // run these tests as if in a desktop
-    // browser with a 720p monitor
     cy.viewport(1440, 900);
   })
 
@@ -24,15 +22,6 @@ describe('UI Test', () => {
     eVChargePage.click_AMPOLEnergyIcon();
     energyPage.click_SWITCHNOW();
     energyPage.verify_Signup_URL();
-    
-    // cy.visit('https://www.ampol.com.au/');
-    // cy.get('.nav-bar--level-2--content > :nth-child(1) > :nth-child(2)').realHover();
-    // cy.get('.amp-mega-menu > :nth-child(2) > :nth-child(1)').realHover();
-    // cy.contains('EV Charging').click();
-    // cy.url().should('include', 'https://ampcharge.ampol.com.au');
-    // cy.get('.nav-bar--level-1--content--brands > a[href="https://energy.ampol.com.au/"]').click();
-    // cy.get('.Hero-module_buttonContainer__sNadp > [data-testid="Button"]').click();
-    // cy.url().should('include', 'https://energy.ampol.com.au/sign-up/postcode');
   })
 
   it('senario_2', () => {
@@ -47,16 +36,5 @@ describe('UI Test', () => {
         cy.writeFile(Cypress.env("interceptResponse_filepath"), interception.response);
         cy.visit(Cypress.env("target_url") + interception.response.body.leadId);
     })
-
-    // cy.intercept('POST', 'https://api.ampolenergy.com.au/onboarding/v1.0/lead').as('getResponse');
-    // cy.visit(Cypress.env("energy_url"));
-    // cy.get('[data-testid="postcode-input"]').type("4011");
-    // cy.get('[data-testid="postcode-option-0"] > .AddressAutocomplete_item__dbUr6').click();
-    // cy.get('[data-testid="Button"]').click();
-    // cy.wait('@getResponse').then((interception)=> {
-    //     cy.writeFile("intercept_result.json", interception.response);
-    //     cy.visit("https://energy.ampol.com.au/sign-up/agent?leadid=" + interception.response.body.leadId);
-    // })
-
   })
 })
